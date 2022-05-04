db246cc7a61dec679cf5a7b11eafd74c18496a43
YoussefElmoudene <elmoudene.ysf@gmail.com>
2022-05-04 18:22:25 +0000

    initial commit

--- src/main/java/com/example/funlearnacademy/WS/admin/AdminAdminWs.java
+++ src/main/java/com/example/funlearnacademy/WS/admin/AdminAdminWs.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping
+@RequestMapping("/admin/admin")
 public class AdminAdminWs {
     @Autowired
     private AdminService adminService;

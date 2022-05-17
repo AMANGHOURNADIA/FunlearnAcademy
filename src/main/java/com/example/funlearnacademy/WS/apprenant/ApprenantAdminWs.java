c003b2651ba77ad2ae7218a55e6506fbe00c932f
YoussefElmoudene <elmoudene.ysf@gmail.com>
2022-05-17 12:56:23 +0100

    initial commit

--- src/main/java/com/example/funlearnacademy/WS/apprenant/ApprenantAdminWs.java
+++ src/main/java/com/example/funlearnacademy/WS/apprenant/ApprenantAdminWs.java
@@ -3,7 +3,6 @@ package com.example.funlearnacademy.WS.apprenant;
 import com.example.funlearnacademy.bean.Admin;
 import com.example.funlearnacademy.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
